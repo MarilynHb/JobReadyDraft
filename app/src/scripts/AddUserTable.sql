0c10db98d1398bc8531ba6f3ae5ff87dcd05ce05
MarilynHb <65784907+MarilynHb@users.noreply.github.com>
2023-04-07 20:02:35 +0300

    update user table
    merged needed fields into the same table. Not needed fields weren't added now, could be added in the future.

--- app/src/scripts/AddUserTable.sql
+++ app/src/scripts/AddUserTable.sql
@@ -1,8 +1,12 @@
 CREATE TABLE User (
 	Id bigint NOT NULL AUTO_INCREMENT,
-	Email nvarchar(20) NOT NULL,
-	Name nvarchar(20) NOT NULL,
-	Password nvarchar(20) NULL,
-	RegistrationDate datetime(7),
+	Email varchar(50) NOT NULL,
+	Name varchar(30) NOT NULL,
+	Password varchar(20) NULL,
+	Headline varchar(20) NULL,
+	About varchar(500) NULL,
+	Location varchar(20) NULL,
+	IndustryId bigint NULL,
+	RegistrationDate datetime(6),
 	PRIMARY KEY (Id)
 );
