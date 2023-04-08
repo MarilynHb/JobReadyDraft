5ccd9e44e7f3e6ef91561b3ea38ed57c619bcbd5
marilyn <habermarilyn2002@gmail.com>
2023-04-08 23:52:22 +0300

    add login feature

--- app/src/main/java/com/example/jobreadydraft/MainActivity.java
+++ app/src/main/java/com/example/jobreadydraft/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.jobreadydraft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int LOADING_TIME = 3000; // in milliseconds
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, LOADING_TIME);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
     }
 }
